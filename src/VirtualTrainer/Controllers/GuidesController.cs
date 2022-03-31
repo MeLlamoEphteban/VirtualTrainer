eb221126da3a04a76fcb2c21bf484678cf032b88
Alexandru Tone <gameforce109@gmail.com>
2022-03-31 20:10:08 +0300

    Added GuideController with views

---
+++ src/VirtualTrainer/Controllers/GuidesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VirtualTrainer;
+
+namespace VirtualTrainer.Controllers
+{
+    public class GuidesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PushPullLegsV1()
+        {
+            return View();
+        }
+
+        public IActionResult PushPullLegsV2()
+        {
+            return View();
+        }
+
+        public IActionResult OneDayGroup()
+        {
+            return View();
+        }
+
+        public IActionResult CompoundGroup()
+        {
+            return View();
+        }
+    }
+}
